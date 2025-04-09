915419cf565dcbade4a9b26151f084cdc8f1e19d
Luigi Albino <luigialbino@hotmail.it>
2025-04-09 11:50:40 +0200

    importo progetto

---
+++ src/main/java/it/links/prenotazione_domain/PrenotazioniDomainApplication.java
@@ -0,0 +1,13 @@
+package it.links.prenotazione_domain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrenotazioniDomainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrenotazioniDomainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/links/prenotazione_domain/PrenotazioniDomainApplicationTests.java
@@ -0,0 +1,13 @@
+package it.links.prenotazione_domain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrenotazioniDomainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
